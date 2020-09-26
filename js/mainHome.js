d1584bf15d15dc87da5e8619dbe934054f299f38
María Jiménez <jmmariafernanda15@gmail.com>
2020-09-26 17:14:09 +0200

    hero html + css

--- js/mainHome.js
+++ js/mainHome.js
@@ -1,4 +1,25 @@
+/* BURGUER MENU */
+    const $navbarMenu = document.getElementById("navbar");
+    const $btnMenu = document.getElementById("btnMenu");
+
 const showMenu = () => {
-    let navbarMenu = document.getElementById("navbar");
-    navbarMenu.classList.toggle('navbar_menu_open');
+    $navbarMenu.classList.toggle('navbar_menu_open');
+
+    if ($navbarMenu.classList.contains("navbar_menu_open")) {
+        $btnMenu.src = "assets/close.svg"
+    } else{
+        $btnMenu.src = "assets/burger.svg"
+    }
 }
+
+/** NAVBAR ICONS EVENTS **/
+
+const $btnCrear = document.getElementById('btnCrear');
+
+$btnCrear.addEventListener('mouseover', () => {
+    $btnCrear.src = "assets/CTA-crear-gifo-hover.svg";
+}  )
+
+$btnCrear.addEventListener('click', () => {
+    $btnCrear.src = "assets/CTA-crear-gifo-active.svg"
+})
