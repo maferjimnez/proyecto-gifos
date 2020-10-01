a5a24d498f17f2d9deed0f813bbb07f5de6d5149
María Jiménez <jmmariafernanda15@gmail.com>
2020-10-01 07:46:57 +0200

    .

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
 const apiKey = 'api_key=rIy2QYy7YdMxdD34hDMu5RewU6tjJ43J';
 const searchEndpointWithApiKey = 'https://api.giphy.com/v1/gifs/search?api_key=rIy2QYy7YdMxdD34hDMu5RewU6tjJ43J&q=';
 const trendingEndpointWithApiKey = 'https://api.giphy.com/v1/gifs/trending?api_key=rIy2QYy7YdMxdD34hDMu5RewU6tjJ43J';
-const searchSyggestion = 'https://api.giphy.com/v1/tags/related/'
+const searchSuggestion = 'https://api.giphy.com/v1/tags/related/'
