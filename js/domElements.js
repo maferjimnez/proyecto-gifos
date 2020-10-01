36b2b200ba770f428d67a5c5464db7998ba6e141
María Jiménez <jmmariafernanda15@gmail.com>
2020-10-01 10:22:23 +0200

    Ordeno elementos del dom

---
+++ js/domElements.js
@@ -0,0 +1,17 @@
+// * *      NAVBAR ELEMENTS       * * //
+const $navbarMenu = document.getElementById("navbar");
+const $btnMenu = document.getElementById("btnMenu");
+
+
+// * *      SEARCH ELEMENTS       * * //
+const $searchInput = document.getElementById('search_input');
+const $btnSearch = document.getElementById('searchIcon');
+const $searchResultGallery = document.getElementById('search__results__gallery');
+const $searchTitle = document.getElementById('search__results__title');
+const $btnShowMore = document.getElementById('show_more_btn');
+const $searchResultContainer = document.querySelector('.search__results__container');
+const $searchErrorContainer = document.querySelector('.search__error__container');
+
+// search suggestions
+const $recomendedSearchContainer = document.querySelector('.recommendedSearch__container');
+const $recomendedSearch = document.querySelector('.recommended__search');
